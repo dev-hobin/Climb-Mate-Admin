d794dcf9030529738a9d1ff05e07e614f72ad8c5
Dev-Hobin <devhobin@naver.com>
2021-04-21 18:31:40 +0900

    배너 이미지 업로드 기능 프론트 영영 80% 완료

--- src/js/controller/BannerController.js
+++ src/js/controller/BannerController.js
@@ -1,13 +1,25 @@
 import HeaderView from '../view/HeaderView';
 import SidebarView from '../view/SidebarView';
+import BannerImageUploadView from '../view/BannerImageUploadView.js';
+
+import ImageUploadModel from '../model/ImageUploadModel.js';
 
 const tag = '[BannerController]';
 
 const BannerController = class {
   constructor() {
+    // 배너 이미지 데이터
+    this._initialBannerImageArray = [];
+    this._bannerImageArray = [];
+    this._deletedImageArray = [];
+
     // 뷰
     this._headerView = new HeaderView();
     this._sidebarView = new SidebarView();
+    this._bannerImageUploadView = new BannerImageUploadView();
+
+    // 모델
+    this._imageUploadModel = new ImageUploadModel();
   }
 
   /* 인터페이스 */
@@ -25,6 +37,13 @@ const BannerController = class {
       .setup(document.querySelector(`[data-sidebar]`))
       .on('@toggleSideMenu', event => this._toggleSideMenu(event.detail));
 
+    this._bannerImageUploadView //
+      .setup(document.querySelector(`[data-uploader="banner"]`))
+      .on('@addImages', event => this._addImages(event.detail))
+      .on('@changeImageLocation', event => this._changeImageLocation(event.detail))
+      .on('@deleteImage', event => this._deleteImage(event.detail))
+      .on('@uploadImages', () => this._uploadImages());
+
     this._lifeCycle();
   };
 
@@ -32,17 +51,30 @@ const BannerController = class {
 
   // 라이프 사이클
   _lifeCycle = async () => {
+    /* 사이드바 메뉴 설정 */
     // * 일단 배너 사진 페이지로 들어왔다고 가정 -> 나중에는 url 값 받아서 구분해야함
     this._sidebarView.initMenu({
       depth1: 'centerInfo',
       depth2: 'banner',
     });
+
+    /* 배너 이미지 설정 */
+    // 1. 이미 등록되어 있는 이미지 url 요청
+    const currentBannerUrls = await this._imageUploadModel.getBannerImages();
+    console.log('이미 등록되어있는 사진 정보 가져오기');
+    // 2. 컨트롤러 배너 이미지 리스트 업데이트
+    this._initialBannerImageArray.push(...currentBannerUrls);
+    this._bannerImageArray.push(...currentBannerUrls);
+    // 3. 뷰에 이미지 아이템 리스트 초기화
+    this._bannerImageUploadView.initItems(currentBannerUrls);
+    console.log('이미지 리스트에 아이템 설정');
   };
 
   // 헤더 어드민 메뉴 토글
   _toggleAdminMenu = () => {
     this._headerView.toggleAdminMenu();
   };
+
   // 사이드바 토글
   _toggleSidebar = () => {
     this._sidebarView.toggleSidebar();
@@ -50,6 +82,84 @@ const BannerController = class {
   // 사이드 메뉴 토글
   _toggleSideMenu = ({ menu }) => {
     this._sidebarView.toggleSideMenu(menu);
+  };
+
+  // 이미지 추가
+  _addImages = async ({ fileList }) => {
+    // 추가한 파일들 유효성 검사하여 유효성 검사 통과한 이미지 파일들과 유효성 검사 실패한 이유가 담긴 에러 리스트 반환
+    const [validatedImageFiles, errorList] = this._imageUploadModel.vaildateImageFiles(
+      fileList,
+      this._bannerImageArray.length
+    );
+    console.log('이미지 유효성 검사 통과한 것들만 가져온다');
+
+    if (validatedImageFiles.length !== 0) {
+      // 현재 이미지 배열 업데이트
+      this._bannerImageArray.push(...validatedImageFiles);
+      // 뷰에 아이템 추가 요청
+      this._bannerImageUploadView.addItems(validatedImageFiles);
+      console.log('이미지 리스트에 아이템들 추가');
+    }
+
+    if (errorList.length === 0) return;
+    // 유효성 검사 통과 못한 파일들 있다면 에러 출력
+    console.log(errorList);
+  };
+  // 이미지 삭제
+  _deleteImage = ({ index }) => {
+    // 현재 이미지 배열 업데이트하고 삭제될 이미지 따로 모은다
+    const deletedImages = this._bannerImageArray.splice(index, 1);
+    // 삭제될 이미지 리스트 업데이트
+    this._deletedImageArray.push(...deletedImages);
+    // 업데이트 완료 됐으면 뷰에게 아이템 삭제하라고 요청한다
+    this._bannerImageUploadView.removeItem(index);
+    console.log('이미지 삭제');
+  };
+  // 이미지 자리 변경
+  _changeImageLocation = ({ beforeIndex, afterIndex }) => {
+    // 원래 배열에서 드래그한 이미지 빼고 인덱스 앞으로 당긴다
+    const draggedItemArray = this._bannerImageArray.splice(beforeIndex, 1);
+    // 자리 이동한 인덱스에 아이템 추가후 다른것들 뒤로 민다
+    this._bannerImageArray.splice(afterIndex, 0, ...draggedItemArray);
+
+    console.log('자리 변경 전');
+    console.log(this._bannerImageArray);
+    console.log('자리 변경 후');
+    console.log(this._bannerImageArray);
+  };
+  // 이미지 업로드
+  _uploadImages = () => {
+    if (!this._checkImagesChange(this._initialBannerImageArray, this._bannerImageArray))
+      return console.log('이미지 변경사항 없음');
+    console.log('이미지 업로드 진행');
+    const orderedList = this._bannerImageArray.map((image, index) => {
+      if (typeof image === 'object') return { order: index + 1, file: image };
+      else return { order: index + 1, url: image };
+    });
+    console.log('순서 정보 추가');
+    console.log(orderedList);
+
+    const files = orderedList.filter(imageObj => imageObj.hasOwnProperty('file'));
+    const urls = orderedList.filter(imageObj => imageObj.hasOwnProperty('url'));
+    const willDeleted = this._deletedImageArray.filter(image => typeof image === 'string');
+
+    console.log('업로드할 파일만 뽑아낸 정보');
+    console.log(files);
+    console.log('이미 등록된 url만 뽑아낸 정보');
+    console.log(urls);
+    console.log('삭제할 이미지');
+    console.log(willDeleted);
+  };
+  // 이미지들을 수정했는지 안했는지 확인
+  _checkImagesChange = (initialImages, currentImages) => {
+    // 1. 배열 길이 비교 -> 다르면 무조건 달라진 것
+    if (initialImages.length !== currentImages.length) return true;
+    // 2. 하나 하나 비교
+    for (const [index, image] of currentImages.entries()) {
+      if (initialImages[index] === image) continue;
+      else return true;
+    }
+    return false;
   };
 };
 
---
+++ src/js/model/ImageUploadModel.js
@@ -0,0 +1,87 @@
+import Model from '../core/Model.js';
+
+const tag = '[ImageUploadModel]';
+
+const ImageUploadModel = class extends Model {
+  constructor() {
+    super();
+  }
+
+  getBannerImages = async (centerId = '') => {
+    // 미리 이미지 아이템이 등록되있다고 가정한 더미 데이터
+    return [
+      'http://placehold.it/200x200.jpg/ff0000/ffffff?text=1',
+      'http://placehold.it/200x200.jpg/800000/ffffff?text=2',
+      'http://placehold.it/200x200.jpg/808000/ffffff?text=3',
+      'http://placehold.it/200x200.jpg/008080/ffffff?text=4',
+      'http://placehold.it/200x200.jpg/800080/ffffff?text=5',
+      'http://placehold.it/200x200.jpg/0000ff/ffffff?text=6',
+      'http://placehold.it/200x200.jpg/F4A460/ffffff?text=7',
+      'http://placehold.it/200x200.jpg/FFB6C1/ffffff?text=8',
+      'http://placehold.it/200x200.jpg/87CEFA/ffffff?text=9',
+      'http://placehold.it/200x200.jpg/F0E68C/ffffff?text=10',
+    ];
+  };
+
+  vaildateImageFiles = (fileList, initialCount) => {
+    if (fileList.length === 0) return [];
+
+    let currentImageCount = initialCount;
+
+    const validatedFiles = [];
+    const errorList = [];
+
+    for (const file of fileList) {
+      // 1. 사진 개수 검사 (최대 30장)
+      if (!this._checkImageCount(currentImageCount)) {
+        errorList.push({
+          title: '사진 업로드 허용 개수 초과',
+          description: '사진은 최대 30장까지 업로드 가능합니다',
+        });
+        break;
+      }
+      // 2. 확장자 검사
+      if (!this._checkImageExtension(file.name)) {
+        errorList.push({
+          title: `허용된 파일 형식이 아닙니다 (${file.name})`,
+          description: '사진 업로드는 .jpg, .png, .jpeg 형식의 파일만 가능합니다',
+        });
+        continue;
+      }
+      // 3. 용량 검사
+      if (!this._checkImageSize(file.size)) {
+        errorList.push({
+          title: `사진 업로드 허용 용량 초과 (${file.name})`,
+          description: '10MB 이하의 사진 파일만 업로드 가능합니다',
+        });
+        continue;
+      }
+      validatedFiles.push(file);
+    }
+
+    return [validatedFiles, errorList];
+  };
+
+  _checkImageCount = count => {
+    if (count >= 30) return false;
+    else return true;
+  };
+  _checkImageExtension = fileName => {
+    const extentions = ['jpeg', 'JPEG', 'jpg', 'JPG', 'png', 'PNG'];
+    const splittedNames = fileName.split('.');
+    // 파일 확장자 제대로 안나뉘어진 경우
+    if (splittedNames.length <= 1) return false;
+    // 현재 파일 확장자
+    const fileExtention = splittedNames[splittedNames.length - 1];
+    // 허용한 확장자인지 확인
+    if (!extentions.includes(fileExtention)) return false;
+    else return true;
+  };
+  _checkImageSize = fileSize => {
+    const maxSize = 10 * 1024 * 1024;
+    if (fileSize > maxSize) return false;
+    else return true;
+  };
+};
+
+export default ImageUploadModel;
